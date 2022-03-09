781a9b4471eca2382cbc0eb65ff9bb08046a316e
Alexander Barth <barth.alexander@gmail.com>
2022-03-09 21:38:29 +0100

    reduce memory

--- test/test_DINCAE_SST.jl
+++ test/test_DINCAE_SST.jl
@@ -12,10 +12,12 @@ Atype = Array{F}
 
 Random.seed!(123)
 
-filename = "avhrr_sub_add_clouds_small.nc"
+#filename = "avhrr_sub_add_clouds_small.nc"
+filename = "avhrr_sub_add_clouds_n10.nc"
 
 if !isfile(filename)
-    download("https://dox.ulg.ac.be/index.php/s/b3DWpYysuw6itOz/download", filename)
+#    download("https://dox.ulg.ac.be/index.php/s/b3DWpYysuw6itOz/download", filename)
+    download("https://dox.ulg.ac.be/index.php/s/2yFgNMkpsGumVSM/download", filename)
 end
 
 
@@ -31,13 +33,10 @@ data_test = data;
 data_all = [data,data_test]
 
 epochs = 2
-batch_size = 50
+batch_size = 5
 save_each = 10
-save_model_each = 500
-skipconnections = [1,2,3,4]
-dropout_rate_train = 0.3
-truth_uncertain = false
-enc_nfilter_internal = round.(Int,32 * 2 .^ (0:4))
+skipconnections = [1,2]
+enc_nfilter_internal = round.(Int,32 * 2 .^ (0:3))
 clip_grad = 5.0
 regularization_L2_beta = 0
 save_epochs = [epochs]
@@ -74,14 +73,3 @@ for (upsampling_method,is3D,truth_uncertain) = ((:nearest,false,false),
     rm(fnames_rec[1])
 end
 
-
-#=
-if haskey(ENV,"CI")
-    # Volks-Wagen
-    @test losses[end] ≈ 23.8846822232148 rtol=1e-4
-    @show losses[end]
-else
-    @test losses[end] == 23.8846822232148
-end
-
-=#
