e8556aab65ba365cf37bf75177bd084b1640663b
Alexander Barth <barth.alexander@gmail.com>
2022-03-09 21:39:01 +0100

    add test_reconstruct_points.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,11 @@ ENV["CUDA_VISIBLE_DEVICES"]=""
 using DINCAE
 using Test
 
-@testset "DINCAE.jl" begin
+@testset "reconstruct gridded data" begin
     include("test_DINCAE_SST.jl")
+end
+
+
+@testset "reconstruct point cloud" begin
+    include("test_reconstruct_points.jl")
 end
